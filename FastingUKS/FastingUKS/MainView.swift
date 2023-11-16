5d65b4f2dda0c13dabe2ddd6eed57e5adb23ec6c
UKS <skulptorrr@gmail.com>
2023-11-16 06:55:24 +0200

    MainView minor fixes

--- FastingUKS/FastingUKS/MainView.swift
+++ FastingUKS/FastingUKS/MainView.swift
@@ -14,14 +14,15 @@ struct MainView: View {
             FastingProgressView(progress: model.progress, fastingType: model.fastingType, timeLeft: model.timeLeft)
         }
         .makeFullyIntaractable()
-        .gesture( TapGesture(count: 2).onEnded { twoTaps() } )
-        .sheet(sheet: model.sheet)
         .onReceive(timer) { _ in
             model.refresh()
         }
         .fillParent()
         .backgroundGaussianBlur(type: .behindWindow, material: .m1_hudWindow)
         .frame(minWidth: 250, idealWidth: 250, minHeight: 280, idealHeight: 280)
+        .dragWndWithClick()
+        .gesture( TapGesture(count: 2).onEnded { twoTaps() } )
+        .sheet(sheet: model.sheet)
         
     }
 }
